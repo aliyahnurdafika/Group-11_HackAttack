83a38aedbac29e7cb0f4ccccc0e260a2e0f7cdf1
Edgar Damien Sipayung <edgarspy@Edgars-MacBook-Air.local>
2025-02-28 20:02:48 -0800

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-4 bg-gray-800 text-white min-h-screen w-full text-center">
+      <div className="text-4xl">VITAMIN.IO</div>
+      <div className="text-xl">Make your recipe from scratch!</div>
+    </div>
+  )
+}
+
+export default App
